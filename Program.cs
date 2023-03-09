61489dd5d66377a6e598fb40605257f3c0b77ad7
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-09 19:31:46 +0100

    final commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,36 @@
 ﻿
-
+// Part 1
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine($"The value of i is {i}");
 }
+Console.WriteLine();
+Console.WriteLine();
+
+// Part 2
+string data = "Tim,Sue,Bob,Jane,Frank";
+List<string> firstNames = data.Split(',').ToList();
+
+for (int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine(firstNames[i]);
+}
+Console.WriteLine();
+Console.WriteLine();
+
+// Part 3
+List<decimal> charges = new();
 
 
+charges.Add(23.78M);
+charges.Add(15.89M);
+charges.Add(125M);
+
+decimal total = 0;
+
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+}
+
+Console.WriteLine($"Our total charge is {total}");
